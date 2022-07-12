5357dc3a1cd409ddab9965976105b00d2d890860
MatveyRubets <rmatvey11@gmail.com>
2022-07-12 15:37:52 +0200

    minor

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,4 +1,5 @@
 import css from './index.module.css';
+import PropTypes from 'prop-types';
 
 import Stats from './Stats';
 import Description from './Description';
@@ -22,6 +23,18 @@ const Profile = ({
       <Description stats={stats} />
     </div>
   );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
 };
 
 export default Profile;
--- src/components/TransactionHistory/index.jsx
+++ src/components/TransactionHistory/index.jsx
@@ -34,6 +34,9 @@ TransactionHistory.propTypes = {
   items: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
     }).isRequired
   ).isRequired,
 };
